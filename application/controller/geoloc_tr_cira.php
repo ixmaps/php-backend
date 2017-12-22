12ae0f3c4bb93cadd7d01f182d71b34c197063c2
Colin <colindmccann@gmail.com>
2017-12-22 14:55:05 +0100

    fully encapsulated the error handling, ready to move things into models

--- application/controller/geoloc_tr_cira.php
+++ application/controller/geoloc_tr_cira.php
@@ -4,7 +4,6 @@ include('../config.php');
 include('../model/GatherTr.php');
 include('../model/IXmapsMaxMind.php');
 
-// move this to... somewhere. DB? config?
 $ptrJsonStructureStr = '{
   "request_id": 123456789,
   "ipt_timestamp": "2017-12-31 23:59:59",
@@ -45,43 +44,28 @@ $ptrJsonStructureStr = '{
   ]
 }';
 // the archetypal json structure for input
+// move this structure to... somewhere. DB? config?
 $ptrJsonStructure = json_decode($ptrJsonStructureStr, TRUE);
-
 // received json
 $hopData = json_decode($_POST["hop_data"], TRUE);
 
-// status starts as 201 (success) and can be modified
-$statusCode = 201;
-// $statusCode of 401 = missing key
-// $statusCode of 402 = missing value
+/***
+ *** construct the basic GEO JSON object
+ ***/
+$geoJson = array();
+
 
 /***
- *** validate the received JSON (eventually put this as a modal class in model CIRAsomename)
+ *** validate the received JSON, if invalid immediately return with error code
  ***/
-
-// 1. confirm that all required keys are present in the submission
-$missingKey = '';
-foreach ($ptrJsonStructure as $key => $value) {
-  if (is_null($_POST[$key])) {
-    $missingKey = $key;
-    $statusCode = 401;
-  }
+// status of each validate function is an array of structure:
+// statusCode: 123,
+// statusMsg: 'abc'
+// success status are 2xx. Only current success status is 201
+$statusObj = validateInputPTR($ptrJsonStructure);
+if ($statusObj["code"] != 201) {        // TODO make 2xx
+  returnGeoJson($geoJson, $statusObj);
 }
-// 2. confirm that all keys are not blank (TODO: do we want to include this error check?)
-$missingValueForKey = '';
-foreach ($_POST as $key => $value) {
-  if (empty($_POST[$key])) {
-    $missingValueForKey = $key;
-    $statusCode = 402;
-  }
-}
-// TODO - we want to move this out to validateInputPTR($ptrJsonStructure)
-// but if so:
-// - is there a hierarchy of error codes? if no, it has to be an obj / array
-// - remember that this needs to also accom the 'message' value
-// - perhaps this wants to be combined with the appendSuccessStatus function
-
-
 
 /***
  *** send the json to ingest_tr_cira (phase 2)
@@ -90,29 +74,27 @@ foreach ($_POST as $key => $value) {
 
 
 /***
- *** construct the basic GEO-JSON return
+ *** construct the GEO-JSON return
  ***/
 
 // create the mm object
 $mm = new IXmapsMaxMind();
 
 // figure out which pass we want to use, *now just using first pass*.
-// we cannot necessarily assume the hop_data array is ordered (eg pass1 != hopdata[0])
+// NB: we cannot necessarily assume the hop_data array is ordered (eg pass1 != hopdata[0])
 // $trByHop = GatherTr::analyzeIfInconsistentPasses($trData); maybe? And others
 $chosenPass = $hopData[0];
 $hops = $chosenPass["hops"];
 
-// construct the header type stuff for GEO-JSON return
-$geoJson = array(
-  "request_id" => $_POST["request_id"],
-  "ixmaps_id" => 0,
-  "hop_count" => count($hops),
-  "terminate" => $chosenPass["terminate"],
-  "boomerang" => checkIfBoomerang($hops, $mm)
-);
+// add the header type stuff for GEO-JSON return
+$geoJson["request_id"] = $_POST["request_id"];
+$geoJson["ixmaps_id"] = 0;
+$geoJson["hop_count"] = count($hops);
+$geoJson["terminate"] = $chosenPass["terminate"];
+$geoJson["boomerang"] = checkIfBoomerang($hops, $mm);
 
 // add the lat/long data to each hop of GEO-JSON
-// I assume we can do this with GatherTr.php, so just some dummy data here
+// (I assume we can do this with GatherTr.php, so just some dummy data here)
 // 1. see if we have the IP in the DB
 
 // 2. see if Maxmind has the IP
@@ -139,22 +121,14 @@ $geoJson["overlay_data"] = $overlayData;
 
 // 3 some kind of default if MM doesn't have it
 
-
-
-// add boomerang value to the obj
-
-
 // close MaxMind files
 $mm->closeDatFiles();
 
 
 /***
- *** return the GEO-JSON (or error code) to CIRA
+ *** return the GEO-JSON to CIRA
  ***/
-$geoJson['status'] = appendSuccessStatus($statusCode);
-// think about destroying the geoJson object here is status is not 201, just returning status json?
-header('Content-type: application/json');
-echo json_encode($geoJson);
+returnGeoJson($geoJson);
 
 
 
@@ -165,29 +139,51 @@ echo json_encode($geoJson);
 
 /* move these all to the model - but which model? Probably at least two, CIRA and some kind of general purpose model */
 
-// MOST LIKELY MODEL: CIRA
-// function validateInputPTR($ptrJsonStructure) {
-
-// }
-
-// MOST LIKELY MODEL: ? (something general) ?
-function appendSuccessStatus($statusCode) {
+// MOST LIKELY MODEL: CIRA (will need to $_POST back as a param, I think - not sure how 'superglobal variables' work)
+function validateInputPTR($ptrJsonStructure) {
+  $code = 201;
   $message = '';
 
-  switch ($statusCode) {
+  // note the implied hierarchy here, 401 will be shown first (this might be too implicit)
+  // 2. confirm that all keys are not blank (TODO: do we want to include this error check?)
+  foreach ($_POST as $key => $value) {
+    if (empty($_POST[$key])) {
+      $message = $key;
+      $code = 402;
+    }
+  }
+  // 1. confirm that all required keys are present in the submission
+  foreach ($ptrJsonStructure as $key => $value) {
+    if (is_null($_POST[$key])) {
+      $message = $key;
+      $code = 401;
+    }
+  }
+
+  $statusObj = array(
+    "code" => $code,
+    "message" => $message
+  );
+
+  return $statusObj;
+}
+
+// MOST LIKELY MODEL: ? (something general) ?
+function generateStatusObj($statusObj) {
+  switch ($statusObj["code"]) {
     case 201:
       $message = "Success";
       break;
     case 401:
-      $message = "Malformed JSON, missing key - " . $missingKey;
+      $message = "Malformed JSON, missing key - " . $statusObj["message"];
       break;
     case 402:
-      $message = "Malformed JSON, unset value for key - " . $missingValueForKey;
+      $message = "Malformed JSON, unset value for key - " . $statusObj["message"];
       break;
   }
 
   $statusJson = array(
-    "code" => $statusCode,
+    "code" => $statusObj["code"],
     "message" => $message
   );
   return $statusJson;
@@ -220,6 +216,15 @@ function checkIfBoomerang($hops, $mm) {
   } else {
     return false;
   }
+}
+
+
+// maybe in the CIRA model? Or maybe just leave here?
+function returnGeoJson($geoJson, $statusObj) {
+  $geoJson['status'] = generateStatusObj($statusObj);
+  header('Content-type: application/json');
+  echo json_encode($geoJson);
+  die;                              // is this bad practice?
 }
 
 ?>
