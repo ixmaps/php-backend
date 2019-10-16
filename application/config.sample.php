d2ba5cb5557dab19bf3c3f62481da695e9d96caa
Colin <colindmccann@gmail.com>
2019-10-16 18:41:12 +0200

    taking down tr-gather

--- application/config.sample.php
+++ application/config.sample.php
@@ -13,9 +13,7 @@ try {
   // Connecting, selecting database
   $dbconn = pg_connect("host=localhost dbname=$dbname user=$dbuser password=$dbpassword")
     or die('Could not connect to DB: ' . pg_last_error());
-}
-catch(PDOException $e)
-{
+} catch(PDOException $e) {
   echo $e->getMessage();
 }
 
@@ -25,12 +23,11 @@ $appRootPath = '/var/www/ixmaps';
 $savePath = $appRootPath.'/gm-temp';
 
 #### MaxMind data and include files
-// (IXmaps server)
 $MM_geoip_dir = "/var/www/ixmaps/application/geoip";
 $MM_dat_dir = "/home/ixmaps/ix-data/mm-data";
 
 //////////////////////////////////////////////////////////
-$trNumLimit=500;
+$trNumLimit = 500;
 $ixmaps_debug_mode = true;
 $ixmaps_hands_off_config = array();
 
