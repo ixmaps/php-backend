e1a6bc63c096371e847c45513ba191e4b86c6a8e
Colin <colindmccann@gmail.com>
2019-10-24 14:37:52 +0200

    need to keep the include in the controller for the model... weird way to do it

--- application/controller/map_search.php
+++ application/controller/map_search.php
@@ -4,8 +4,10 @@ ini_set( "display_errors", 0); // force show errors for debug
 
 include('../config.php');
 include('../model/MapSearch.php');
+include('../model/Traceroute.php');
 
 $debugTrSearch = false;
+
 $r = MapSearch::countTrs($_POST);
 
 echo json_encode($r);
