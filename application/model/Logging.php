1d82ad6d0791ebfb52c00a845ce17e1444e38452
Colin <colindmccann@gmail.com>
2020-11-22 14:40:09 -0500

    actually added the logging class now

---
+++ application/model/Logging.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *
+ *
+ * @author IXmaps.ca (Colin)
+ * @since 2020 Nov
+ *
+ */
+class Logging
+{
+  private $logFile;
+  private $startTime;
+
+  function __construct() {
+    global $searchLog;
+
+    $this->startTime = $this->getNow();
+    $this->logFile = fopen($searchLog, "a+") or exit("Unable to open file!");
+    fwrite($this->logFile, "\n");
+  }
+
+  public function search($message) {
+    fwrite($this->logFile, $message." - ".$this->executionTime()."\n");
+  }
+
+  public function __destruct()
+    {
+      fwrite($this->logFile, "----------------------------------------\n");
+      fclose($this->logFile);
+    }
+
+  public function executionTime() {
+    $endTime = Logging::getNow();
+    $totalTime = ($endTime - $this->startTime);
+    return number_format($totalTime, 2);
+  }
+
+  private function getNow() {
+    $mtime = microtime();
+    $mtime = explode(" ",$mtime);
+    return $mtime[1] + $mtime[0];
+  }
+
+}
