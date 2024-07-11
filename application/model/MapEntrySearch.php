9448939f230facf6e8034629b4e5c988793087cc
Colin <colindmccann@gmail.com>
2024-07-11 13:08:17 -0400

    dumber but more performant map entry search

--- application/model/MapEntrySearch.php
+++ application/model/MapEntrySearch.php
@@ -45,87 +45,58 @@ class MapEntrySearch
         "results" => array(),
         "total" => 0,
       );
+    }
 
-    } else {
-      $sqlBase = "SELECT DISTINCT traceroute_traits.traceroute_id FROM annotated_traceroutes, traceroute_traits WHERE annotated_traceroutes.traceroute_id = traceroute_traits.traceroute_id";
+    $sqlBase = "SELECT DISTINCT traceroute_traits.traceroute_id FROM annotated_traceroutes, traceroute_traits WHERE annotated_traceroutes.traceroute_id = traceroute_traits.traceroute_id";
 
-      $constraintNum = 0;
-      $sqlParamsArray = array();
-      $constraintWhereParams = array(); // build count independent constraint
-      $intersectWhereParams = array(); // build count intersect all constraints
-      $sqlIntersectArray = array();
+    $filterResults = array();
+    // count trs for each of the constraints
+    foreach ($data as $constraintKind => $constraint) {
+      $constraintCount = 0;
 
-      $filterResults = array();
-      // count trs for each of the constraints
-      foreach ($data as $constraintKind => $constraint) {
+      $constr = $constraint["constraint3"];
+      if ($constraint["constraint3"] == "city") {
+        $constr = "mm_city";
+      }
 
-        $constraintWhereParams = Traceroute::buildWhere($constraint);
-        $sqlCount = $sqlBase.$constraintWhereParams[0]; // add where conditions
+      $sqlCount = "SELECT DISTINCT traceroute_id FROM annotated_traceroutes WHERE hop = 1 AND {$constr} = '{$constraint["constraint4"]}';";
+      
+      $result = pg_query($dbconn, $sqlCount) or die('countTrResults: Query failed: incorrect parameters');
+      $trCountArr = pg_fetch_all($result);
+      pg_free_result($result);
+      if ($trCountArr !== false) {
+        $constraintCount = count($trCountArr);
+      }
 
-        $constraintCount = 0;
-        $result = pg_query_params($dbconn, $sqlCount, array($constraintWhereParams[1])) or die('countTrResults: Query failed: incorrect parameters');
-        $trCountArr = pg_fetch_all($result);
-        pg_free_result($result);
-        if ($trCountArr !== false) {
-          $constraintCount = count($trCountArr);
-        }
+      // handle the case of only 1 param being passed
+      $intersectCount = $constraintCount;
 
-        // sql for intersect statements
-        if ($constraintCount != 0) {
-          $constraintNum++;
-          $intersectWhereParams = Traceroute::buildWhere($constraint, $constraintNum);
-          $sqlIntersectArray[] = $sqlBase.$intersectWhereParams[0]; // add sql where
-          $sqlParamsArray[] = $constraintWhereParams[1]; // collect params array
-        }
+      $filterResults[$constraintKind] = array(
+        "total" => $constraintCount,
+        "constraint" => $constraint
+      );
+    } // end for each
 
-        $filterResults[$constraintKind] = array(
-          "total" => $constraintCount,
-          "constraint" => $constraint
-        );
-      } // end for each
+    // for the intersect
+    if (count($data) > 1) {
+      $asnum = $filterResults["myAsn"]["constraint"]["constraint4"];
+      $city = $filterResults["myCity"]["constraint"]["constraint4"];
+      $sqlTotalCount = "SELECT DISTINCT traceroute_id FROM annotated_traceroutes WHERE hop = 1 AND asnum = {$asnum} AND mm_city = '{$city}';";
 
-      // query intersect of the constraints to determine 'Number of routes found that meet all above checked conditions' value
-      $sqlIntersect = "";
-      if (count($sqlIntersectArray) > 1) {
-        $c = 0;
-        foreach ($sqlIntersectArray as $sqlI) {
-          $c++;
-
-          // first item
-          if ($c == 1) {
-            $sqlIntersect.="".$sqlI."";
-          // last item
-          } else {
-            $sqlIntersect.="
-            INTERSECT
-            ".$sqlI."
-            ";
-          }
-        } // end for
-
-        $result = pg_query_params($dbconn, $sqlIntersect, $sqlParamsArray) or die('countTrResults: Query failed: incorrect parameters');
-        $trArrIntersect = pg_fetch_all($result);
-        pg_free_result($result);
-
-        if ($trArrIntersect == false) {
-          $trIdCount = 0;
-        } else {
-          $trIdCount = count($trArrIntersect);
-        }
-
-      } else {
-        // only one constraint
-        $val = array_values($filterResults)[0];
-        $trIdCount = $val['total'];
+      $result = pg_query($dbconn, $sqlTotalCount) or die('countTrResults: Query failed: incorrect parameters');
+      $trTotalCount = pg_fetch_all($result);
+      pg_free_result($result);
+      if ($trTotalCount !== false) {
+        $intersectCount = count($trTotalCount);
       }
     }
 
     ini_set('memory_limit', '128M');
 
     return array(
-             "results" => $filterResults,
-             "total" => $trIdCount,
-           );
+              "results" => $filterResults,
+              "total" => $intersectCount,
+            );
   }
 }
 
